40c8c23627bc6a69a6841a6ea45a69b6c5151434
arglurgl <34896033+arglurgl@users.noreply.github.com>
2022-10-16 19:02:02 +0200

    first test sketch

---
+++ rocketlogger/src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+#include "Adafruit_TinyUSB.h" // needed for auto-reset and auto-upload to work
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 13
+
+void setup()
+{
+   Serial.begin(115200); // needed for auto-reset and auto-upload to work
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // wait for a second
+  delay(50);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+   // wait for a second
+  delay(50);
+}
